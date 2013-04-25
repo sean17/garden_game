354894057f36ddac5491563febf1aad42c2837bb
Sean Zawicki <seanzawi@buffalo.edu>
2013-04-25 13:01:29 -0400

    Uploading Michaels update to index.js

--- index.js
+++ index.js
@@ -1,94 +1,117 @@
 var data = {'url': 'http://www.untiredwithloving.org/palm_tree_2.jpg'};
 var plantList = { 
 	"list": [
-		{'name':'palm', 'scientific':'yo motha', 'image':'http://www.untiredwithloving.org/palm_tree_2.jpg', 'hint1': 'palm', 'hint2': 'its a palm', 'hint3': 'just type palm'},
-		{'name':'rose', 'scientific':'Rosa berberifolia', "image":"http://www.nasa.gov/centers/goddard/images/content/174100main_planet_plants_lg.jpg",'hint1': 'palm', 'hint2': 'its a palm', 'hint3': 'just type palm'},
-		{'name':'cactus', 'scientific':'yo dog', "image":"http://www.nasa.gov/centers/goddard/images/content/174100main_planet_plants_lg.jpg",'hint1': 'palm', 'hint2': 'its a palm', 'hint3': 'just type palm'}
+		{'name':'palm', 'scientific':'yo_motha', 'image':'http://www.untiredwithloving.org/palm_tree_2.jpg', 'hint1': 'palm', 'hint2': 'its a palm', 'hint3': 'just type palm'},
+		{'name':'rose', 'scientific':'Rosa_berberifolia', "image":"http://www.nasa.gov/centers/goddard/images/content/174100main_planet_plants_lg.jpg",'hint1': 'rose', 'hint2': 'titanic', 'hint3': 'iceberg'},
+		{'name':'cactus', 'scientific':'yodog', "image":"http://www.nasa.gov/centers/goddard/images/content/174100main_planet_plants_lg.jpg",'hint1': 'cactus jack', 'hint2': 'its a cact', 'hint3': 'just type cact'}
 		]}
 		
 // $("#gameModal").append('<div class="span3"><img src="'+data.url+'">')
-$("#plantList").empty()
+$("#plantList").empty();
 
 var plantArray = plantList.list;
+var score = 0;
 
 for(var i=0; i<plantArray.length; i++) {
-	// alert(plantArray[i].name)
-	// $("#plantList").append('<div class="span2">')
-    $("#plantList").append("<div class='span2'><a href='#"+plantArray[i].name+"modal'' data-toggle='modal'> <img value='"+plantArray[i].name+"'style='height:200px;' src='"+plantArray[i].image+"'/></a></div>")
-    
-	// $("#plantList").append('</div>')
-	//$("#plantList").append('<a href ="#'+plantArray[i].name+'">');
-	//$("#modalList").append('<div id ="+plantArray[i].name+">');
+    $("#plantList").append("<div class='span2'><a href='#"+plantArray[i].scientific+"modal' data-toggle='modal'> <img value='"+plantArray[i].scientific+"'style='height:200px;' src='"+plantArray[i].image+"'/></a></div>")
+
 }
-//$(".modal-header").append("<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button><h3>Mystery Plant</h3>");
+
 
 
 for(var i=0; i<plantArray.length; i++) {
 //Create modale divs for each plant
 	$("#plantList").append(
-		"<div id= '"+plantArray[i].name+"modal' class='modal hide fade'>"+
-			"<div class='modal-header'>"+
+		"<div id= '"+plantArray[i].scientific+"modal' class='modal hide fade'>"+
+			"<div id='"+plantArray[i].scientific+"modal-header'>"+
           		"<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>"+
-          		"<h3>"+plantArray[i].name+"</h3>"+
+          		"<h3>Mystery Plant</h3>"+
         	"</div>"+
         	"<div class='modal-body'>"+
           		 "<div class='row'>"+
           		 	"<div class='span2' id='hintdiv'> </br>"+
-                	"<div id='hintdiv1'> <input type='button' id ='ht1' class='btn btn-info' data-toggle ='button' value='Hint 1'></a></br></br></br></div>"+
-		        	"<div id='hintdiv2'> <input type='button' id ='ht2' class='btn btn-info' data-toggle ='button' value='Hint 2'></a></br></br></br></div>"+
-		       		"<div id='hintdiv3'> <input type='button' id ='ht3' class='btn btn-info' data-toggle ='button' value='Hint 3'></a></br></br></br></div>"+
+                	"<div id='hintdiv1'> <input type='button' id ='"+plantArray[i].scientific+"h1' class='btn btn-info' data-toggle ='button' value='Hint 1'></a></br></br></br></div>"+
+		        	"<div id='hintdiv2'> <input type='button' id ='"+plantArray[i].scientific+"h2' class='btn btn-info' data-toggle ='button' value='Hint 2'></a></br></br></br></div>"+
+		       		"<div id='hintdiv3'> <input type='button' id ='"+plantArray[i].scientific+"h3' class='btn btn-info' data-toggle ='button' value='Hint 3'></a></br></br></br></div>"+
             	"</div>"+
             	"<div class='span2 offset1'>"+
-               		"<img style='opacity:1000;height:200px;'' src='"+plantArray[i].image+"' />"+
+               		"<img style='opacity:1000;height:200px;'' src='"+plantArray[i].image+"'/>"+
             		"</div>"+
           		"</div>"+
         	"</div>"+
         	"<div class='modal-footer'>"+
         	"<div class='row'>"+
-        		"<div class='span2' align='left'>"+        	
-        			"<b> score: </b>"+
+        		"<div class='span2' id='"+plantArray[i].scientific+"result' align='left'>"+        	
+        			"<b>  </b>"+
            		"</div>"+
-           		"<input type='text'>"+
-           		"<a href='#'' class='btn btn-primary'>Submit</a>"+
+           		"<input id='"+plantArray[i].scientific+"textbox' type='text'>"+
+              "<input type='button' align='right' id='"+plantArray[i].scientific+"submit' class='btn btn-primary' value='Submit'</a>"+
+           		//"<a href='#' id='"+plantArray[i].scientifice+"submit' class='btn btn-primary'>Submit</a>"+
         	"</div>"+
       	"</div>"
       	)
 }
 
 
-//hint button different for each hint
-//function hintList(text){
-	//var elem = document.getElementById("ht1");
-    //elem.value = hint1;
-    //$("#ht1").value = hint1;
-    //if ( elem.value = hint1 ) elem.value = "hint1";
-    //else elem.value = hint1;
-    //text.value = "show some hints";  
-//}
+//--------------Hint Buttons----------------
+for(var i=0; i<plantArray.length; i++){
 
-$("#ht1").click(function(){
-	if ($(this).attr('value') == 'Hint 1') 
-		$(this).attr('value', 'the hint');
-	else
-		$(this).attr('value', 'Hint 1');
-});
+	$("#"+plantArray[i].scientific+"h1").click(function(){
+		var btn = $(this).attr('id');
+			for(var i=0;i<plantArray.length;i++){
+				if(btn ===""+plantArray[i].scientific+"h1"){
+					$(this).attr('value', plantArray[i].hint1);
+          score = score-25;	
+				}
 
-$("#ht2").click(function(){
-	if ($(this).attr('value') == 'Hint 2') 
-		$(this).attr('value', 'the hint');
-	else
-		$(this).attr('value', 'Hint 2');
-});
+			}
+	});
 
-$("#ht3").click(function(){
-	if ($(this).attr('value') == 'Hint 3') 
-		$(this).attr('value', 'the hint');
-	else
-		$(this).attr('value', 'Hint 3');
-});
+  $("#"+plantArray[i].scientific+"h2").click(function(){
+    var btn = $(this).attr('id');
+      for(var i=0;i<plantArray.length;i++){
+        if(btn ===""+plantArray[i].scientific+"h2"){
+          $(this).attr('value', plantArray[i].hint2);
+          score = score-25; 
+        }
 
+      }
+  });
 
-//submit button to check answer
-function submit(){
+  $("#"+plantArray[i].scientific+"h3").click(function(){
+    var btn = $(this).attr('id');
+      for(var i=0;i<plantArray.length;i++){
+        if(btn ===""+plantArray[i].scientific+"h3"){
+          $(this).attr('value', plantArray[i].hint3);
+          score = score-25; 
+        }
 
+      }
+  });
+
+//--------------Submit Buttons----------------
+	$("#"+plantArray[i].scientific+"submit").click(function(){
+      var btnid = $(this).attr('id');
+      for(var i=0;i<plantArray.length;i++){
+        if( btnid === ""+plantArray[i].scientific+"submit"){
+          var guess = document.getElementById(""+plantArray[i].scientific+"textbox").value;
+          var answer = plantArray[i].name;
+          guess = guess.toLowerCase().split(' ');
+          guess = guess[0] + guess[1]
+          answer = answer.toLowerCase().split(' ');
+          answer = answer[0] + answer[1]
+          if(guess===answer){
+            $("#"+plantArray[i].scientific+"result").html('You are correct!');
+            $("#"+plantArray[i].scientific+"modal-header").html('<h3> '+plantArray[i].name+' </h3>');
+            score = score +100;
+            $("#score").html('Total Points: '+score+'');
+            $(this)[0].disabled = true;
+          }
+          else{
+            $("#"+plantArray[i].scientific+"result").html('guess again!') 
+          }
+        }
+      }
+	});
 }
+
