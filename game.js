cbbc92d18bb79ef0c88f0ed0ce1fda019e3f7c04
Sean Zawicki <seanzawi@buffalo.edu>
2013-05-01 08:05:12 -0400

    now using the common name for guessing

--- game.js
+++ game.js
@@ -1,39 +1,62 @@
 $(function()
+{
+
+  totalScore = 0;
+  var totalPointsAvailable = 0;
+  var url = "http://botanicalapp.com/api/v1/";
+  var questions = ""
+
+  function getGameNumber()
   {
-
-    totalScore = 0;
-    var totalPointsAvailable = 0;
-    var url = "http://botanicalapp.com/api/v1/";
-    var questions = ""
-
-    var score = {
-      update: function(){ return false;}
-    }
-    
-    function getGameNumber()
+    var search = window.location.search.substr(1);
+    var params = {};
+    $.each(search.split('&'), function (i, e)
     {
-          var search = window.location.search.substr(1);
-          var params = {};
-          $.each(search.split('&'), function (i, e)
-          {
-            var parts = e.split('=');
+      var parts = e.split('=');
 
-            params[parts[0]] = parts[1];
-          });
+      params[parts[0]] = parts[1];
+    });
 
           // alert(params.id)// params;
-        $.ajax(url + "guessing_games/"+params.id)
-        .success(function ( data ) {
-          console.log(JSON.stringify(data))
-          questions = data.guessing_game.guessing_game_questions
-          totalPointsAvailable = 100*questions.length
-          $("#score").html('<b>Total Score : </b>'+totalScore + " / "+totalPointsAvailable)
-  
+          $.ajax(url + "guessing_games/"+params.id)
+          .success(function ( data ) {
+
+            var common_name = '';
+            var nameList = [];
+
+            console.log(JSON.stringify(data))
+            questions = data.guessing_game.guessing_game_questions
+
             for(var i =0; i < questions.length; i++){
-                $("#matchDiv").append("<div class='span3'><a href='#"+questions[i].guessing_game_question.id+"' data-toggle='modal'> <img alt='aasdfasdfasdf' style='height:200px;' src='"+"http://www.botanicalapp.com/"+ questions[i].guessing_game_question.plant.plant_photos[0].plant_photo.image.url+"'/></a></div>")
+              $.ajax(url + "plants/"+questions[i].guessing_game_question.plant_id)
+              .success(function ( result ) {
+                common_name = result.plant.common_name
+                common_name = common_name.replace(/ /g,'')
+                common_name = common_name.toLowerCase()
+                nameList[i] = common_name
+                $("#sub_"+result.plant.id).attr('name',common_name)
+              })
             }
 
+            totalPointsAvailable = 100*questions.length
+            $("#score").html('<b>Total Score : </b>'+totalScore + " / "+totalPointsAvailable)
+
+            for(var i =0; i < questions.length; i++){
+              if (questions[i].guessing_game_question.plant.plant_photos[0]){
+                $("#matchDiv").append("<div class='span3'><a href='#"+questions[i].guessing_game_question.id+"' data-toggle='modal'> <img alt='aasdfasdfasdf' style='height:200px;' src='"+"http://www.botanicalapp.com/"+ questions[i].guessing_game_question.plant.plant_photos[0].plant_photo.image.url+"'/></a></div>")
+              } else {
+                $("#matchDiv").append("<div class='span3'><a href='#"+questions[i].guessing_game_question.id+"' data-toggle='modal'> <img alt='aasdfasdfasdf' style='height:200px;' src='"+"favicon.png"+"'/></a></div>")
+              }
+            }
+            var plant_photo = ""
+
             for(var i=0; i<questions.length; i++) {
+             if (questions[i].guessing_game_question.plant.plant_photos[0]){
+               plant_photo = "http://www.botanicalapp.com/"+ questions[i].guessing_game_question.plant.plant_photos[0].plant_photo.image.url
+             } else{
+              plant_photo = 'favicon.png'
+            }
+
               //Create modale divs for each plant
               $("#plantList").append(
                 "<div id= '"+questions[i].guessing_game_question.id+"' class='modal hide fade'>"+
@@ -49,7 +72,7 @@ $(function()
                 '<div style="height:50px"> <input type="button" class="btn btn-info" data-toggle ="button" onClick="this.value = this.name; totalScore -=25" name="'+questions[i].guessing_game_question.hint3+'" value="Hint 3"></a></br></br></br></div>'+
                 "</div>"+
                 "<div class='span2 offset1'>"+
-                "<img style='opacity:1000;height:200px;'' src='http://www.botanicalapp.com/"+ questions[i].guessing_game_question.plant.plant_photos[0].plant_photo.image.url+"'/>"+
+                "<img style='opacity:1000;height:200px;'' src='"+ plant_photo+"'/>"+
                 "</div>"+
                 "</div>"+
                 "</div>"+
@@ -59,7 +82,7 @@ $(function()
                 "<b>  </b>"+
                 "</div>"+
                 "<input class='guess' type='text'>"+
-                "<input type='submit' align='right' class='btn btn-primary submit' value='Submit' name='"+questions[i].guessing_game_question.plant.name+"'>"+
+                "<input type='submit' align='right' class='btn btn-primary submit' value='Submit' name=' ' id='sub_"+questions[i].guessing_game_question.plant_id+"'>"+
                             // "<a href='#' id='"+plantArray[i].scientifice+"submit' class='btn btn-primary'>Submit</a>"+
                             "</div>"+
                             "</div>"
@@ -73,23 +96,27 @@ $(".btn-info").click(function(){
 })
 
 $(".submit").click(function(){
-  var a = $('.guess').val()
+  var guess = $('.guess').val()
+  guess = guess.replace(/ /g,'')
+  guess = guess.toLowerCase()
+
     // alert(a)
-    if(a===this.name){
+    if(guess===this.name){
       totalScore += 100
       $("#score").html('<b>Total Score : </b>'+totalScore + " / "+totalPointsAvailable)
-      $(this).parent().html(this.name)
+      $(this).parent().html('You have guessed the correct plant! </br> '+this.name)
       $(this).remove()
+      $("#")
     }
-})
-        }
   })
-  .error(function ( data ) {
-      alert('Sorry, it doesn\'t appear as though things are working properly')
-  });
+}
+})
+.error(function ( data ) {
+  alert('Sorry, it doesn\'t appear as though things are working properly')
+});
 
-  }
+}
 
 
-  getGameNumber()
+getGameNumber()
 });
